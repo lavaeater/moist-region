5c8a89b61f90b5389c6422a32667b34c3b9d592d
Tommie Nygren <tommie.nygren@gmail.com>
2022-07-03 23:50:26 +0200

    Is everything fixed?

--- lwjgl3/src/main/kotlin/moist/core/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/moist/core/lwjgl3/Lwjgl3Launcher.kt
@@ -4,11 +4,11 @@ package moist.core.lwjgl3
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
-import moist.core.MainGame
+import moist.core.MoistGame
 
 /** Launches the desktop (LWJGL3) application. */
 fun main() {
-    Lwjgl3Application(MainGame(), Lwjgl3ApplicationConfiguration().apply {
+    Lwjgl3Application(MoistGame(), Lwjgl3ApplicationConfiguration().apply {
         setTitle("moist-region")
         setWindowedMode(1280, 960)
         setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
